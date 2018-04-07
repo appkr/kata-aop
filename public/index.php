90320c4faaa113c82154f4ae1f18dbc7d53d348a
appkr <juwonkim@me.com>
2018-04-08 01:27:01 +0900

    DI Container 추가

--- public/index.php
+++ public/index.php
@@ -1,8 +1,19 @@
 <?php
 
+use DI\ContainerBuilder;
 use KataAop\Hello;
+use function DI\create;
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-$hello = new Hello();
+$containerBuilder = new ContainerBuilder();
+$containerBuilder->useAutowiring(false);
+$containerBuilder->useAnnotations(false);
+$containerBuilder->addDefinitions([
+    Hello::class => create(Hello::class),
+]);
+
+$container = $containerBuilder->build();
+
+$hello = $container->get(Hello::class);
 echo $hello->aop();
